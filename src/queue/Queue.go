5ea66867867d37e13e6db0307ba0fbc1bfb2eb7d
YoungNovice <2306915539@qq.com>
2018-04-02 23:58:03 +0800

    interface{}

--- src/queue/Queue.go
+++ src/queue/Queue.go
@@ -1,17 +1,28 @@
 package queue
 
-type Queue []int
+type Queue []interface{}
 
-func (q *Queue) Push(v int)  {
+func (q *Queue) Push(v interface{}) {
 	*q = append(*q, v)
 }
 
-func (q *Queue) Pop() int  {
+func (q *Queue) Pop() interface{} {
 	v := (*q)[0]
 	*q = (*q)[1:]
 	return v
 }
 
-func (q *Queue) IsEmpty() bool  {
+func (q *Queue) PushInt(v interface{}) {
+	*q = append(*q, v.(int))
+}
+
+// interface.(type) 转类型
+func (q *Queue) PopInt() int {
+	v := (*q)[0]
+	*q = (*q)[1:]
+	return v.(int)
+}
+
+func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
--- src/queue/entry/entry.go
+++ src/queue/entry/entry.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"queue"
 	"fmt"
+	"queue"
 )
 
 func main() {
 	q := queue.Queue{1}
 	q.Push(1)
+	q.Push("sd")
 	fmt.Println(q.IsEmpty())
 	pop := q.Pop()
 	fmt.Println(pop)
+	fmt.Println(q.Pop())
 	fmt.Println(q.Pop())
 	fmt.Println(q.IsEmpty())
 }
