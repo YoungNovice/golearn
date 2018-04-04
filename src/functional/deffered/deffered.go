ee459158fdc75290a313ae260146233e7f19caa9
YoungNovice <2306915539@qq.com>
2018-04-04 23:03:32 +0800

    错误处理

--- src/functional/deffered/deffered.go
+++ src/functional/deffered/deffered.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"functional/fib"
 	"os"
@@ -9,9 +10,16 @@ import (
 
 // deffer 处理资源
 func WriteFile(fileName string) {
-	file, err := os.Create(fileName)
+	//file, err := os.Create(fileName)
+	file, err := os.OpenFile(fileName, os.O_EXCL|os.O_CREATE, 0666)
+	err = errors.New("this is my errors")
 	if err != nil {
-		panic(err)
+		if pathError, ok := err.(*os.PathError); !ok {
+			fmt.Println(pathError.Op, pathError.Err, pathError.Path)
+		} else {
+			panic(err)
+		}
+		return
 	}
 
 	defer file.Close()
