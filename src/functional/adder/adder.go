a99496acb73cc31f543b94bf5d3976a61828b46e
AlanYoung <2306915539@qq.com>
2018-11-05 21:53:16 +0800

    adder

--- src/functional/adder/adder.go
+++ src/functional/adder/adder.go
@@ -1,40 +1,23 @@
-package adder
+package main
 
 import "fmt"
 
-/**
- func 函数有局部变量v 和 一个自由变量v
-自由变量不是在函数体里面定义的， 他是函数所处的环境
+// 函数类型别名
+type iAdder func(int) (int, iAdder)
 
-
-*/
-
-func adder() func(i int) int {
-	sum := 0
-	return func(v int) int {
-		sum += v
-		return sum
-	}
-}
-
-type iAdder func(i int) (int, iAdder)
-
-func adder2(x int) iAdder {
+// 1.递归调用 2.函数式编程 3.闭包
+func adder(x int) iAdder {
 	return func(temp int) (int, iAdder) {
-		return x + temp, adder2(x + temp)
+		total := x + temp
+		return total, adder(total)
 	}
 }
 
 func main() {
-	var add = adder()
-	for i := 0; i < 10; i++ {
-		fmt.Println(add(i))
-	}
-
-	adder := adder2(0)
+	xxx := adder(0)
 	for i := 0; i < 10; i++ {
 		var sum int
-		sum, adder = adder(i)
+		sum, xxx = xxx(i)
 		fmt.Println(sum)
 	}
 }
